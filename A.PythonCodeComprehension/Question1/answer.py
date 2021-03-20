26c83687650962208f6ac80a2b05342896ac2646
Ruhshan <ruhshan.ahmed@gmail.com>
2021-03-20 11:45:44 +0600

    Update variable names in Question1's answer script

--- A.PythonCodeComprehension/Question1/answer.py
+++ A.PythonCodeComprehension/Question1/answer.py
@@ -2,9 +2,9 @@ from random import shuffle
 
 
 def foo(length):
-    list_for_random = range(1, length + 1)
-    shuffle(list_for_random)
-    return list_for_random
+    list = range(1, length + 1)
+    shuffle(list)
+    return list
 
 
 x = foo(10)
